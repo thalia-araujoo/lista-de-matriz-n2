33d1be764b9b0fc59ed246a482eecf13a2a7fa5a
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-03-09 14:49:45 -0300

    Add files via upload

---
+++ quest 3.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	printf("\n\nMatriz inversa\n");
+	printf("-----------------------------\n");
+	int A[2][2] = {{1,2}, {3, 4}};
+	int dt;
+	int inversa[2][2] = {{A[1][1], -A[0][1]}, {-A[1][0], A[0][0]}};
+	
+	dt = A[0][0] * A[1][1] - A[0][1] * A[1][0];
+	
+	for(int i = 0; i < 2; i++){
+		for(int j = 0; j < 2; j++){
+			inversa[i][j] /= dt;
+		}
+	}
+	printf("\n");
+	printf("A Matriz inversa = [ %.2d  %.2d ]\n", inversa[0][0], inversa[0][1]);
+	printf("				 = [ %.2d  %.2d ]\n", inversa[1][0], inversa[1][1]);
+	
+	
+	return 0;
+}
