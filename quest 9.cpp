b1e0b12520ccf297a7f110417e54449a8190668b
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-03-11 11:43:27 -0300

    Update quest 9.cpp

--- quest 9.cpp
+++ quest 9.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//quest 9 Dada a matriz A abaixo, calcule a matriz de rotação de 90 graus:
+//quest 9 Dada a matriz A abaixo, calcule a matriz de rotaÃ§Ã£o de 90 graus:
 //A = [[1, 2],
 //[3, 4]]
 
@@ -9,6 +9,8 @@ int main(){
 	int i, j;
 	int mat[2][2];
 	
+	printf("\tPreenchendo a matriz\n\n");
+	printf("---------------------------\n\n");
 	for(int i = 0; i < 2; i++){
 		for(int j = 0; j < 2; j++){
 			printf("Informe os valores da matriz: ");
@@ -21,12 +23,13 @@ int main(){
 	printf("\tMATRIZ ROTACIONADA\n");
 	printf("---------------------------\n\n");
 	
-	//usei a logica de trocas de variaveis decrecendo
+	//usei a logica de trocas de variaveis decrecendo, assim ele gira da direita para a esquerda
+	// colunas como se fossem linhas, "girando" a matriz em 90Â°. 
 	for(j = 1; j >= 0; j--)       
 	{
         for(i = 0; i < 2; i++)
         {
-            printf("%d      ", mat[i][j]);
+            printf("%d    ", mat[i][j]);
         }
         
 	}
